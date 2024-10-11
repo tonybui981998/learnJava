16be44017d5ab2b7faff9d030b8be7cc9042dda4
TonyBui <buianh981998@gmail.com>
2024-10-11 15:39:15 +1300

    hello spring

---
+++ hello-spring-boot/src/main/java/com/example/hello_spring_boot/controller/HelloSpringController.java
@@ -0,0 +1,12 @@
+package com.example.hello_spring_boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringController {
+    @GetMapping("/hello")
+    String sayHello(){
+        return  "Welcome spring boot with tony";
+    }
+}
